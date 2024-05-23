890ab5dceedeb5ad5da47cd4dfaf8d49f60a0dac
LasseHindsberg <lassehindsberg@gmail.com>
2024-05-23 12:25:03 +0200

    added datetime to be preset to current tme

--- ForecastLibExam/Models/Forecast.cs
+++ ForecastLibExam/Models/Forecast.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public double? Temp { get; set; }
         public double? Humidity { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
 
         public override string ToString()
         {
